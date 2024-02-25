e96d305887cb3ef2dafebb3b5291e1681c7ca13c
Josh Parker <joshBBparker@gmail.com>
2024-02-25 16:34:19 -0700

    add the rotated badge symbol view

--- ios-swift/Landmarks/Landmarks/Views/BadgeSymbol.swift
+++ ios-swift/Landmarks/Landmarks/Views/BadgeSymbol.swift
@@ -9,75 +9,41 @@ import SwiftUI
 
 struct BadgeSymbol: View {
     var body: some View {
-        let height: CGFloat = 150.0
-        let width: CGFloat = 250.0
-        let topHeight = height / 3
-        
-        ZStack {
-            Path{ path in
-                path.move(to: CGPoint(x: width / 2, y: topHeight / 2))
-                let top = [
-                    CGPoint(
-                        x: 0.5 * width,
-                        y: 0.0 * topHeight
-                    ),
-                    CGPoint(
-                        x: 0.71 * width,
-                        y: 1.2 * topHeight
-                    ),
-                    CGPoint(
-                        x: 0.5 * width,
-                        y: 0.75 * topHeight
-                    ),
-                    CGPoint(
-                        x: 0.29 * width,
-                        y: 1.2 * topHeight
-                    ),
-                ]
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width * 0.5
+                let topWidth = width * 0.226
+                let topHeight = height * 0.488
                 
-                top.forEach{ point in
-                    path.addLine(to: point)
-                }
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing)
+                ])
                 
-                path.addLine(to: top[0])
+                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+                path.addLines([
+                    CGPoint(x: middle , y: topHeight / 2 + spacing * 3),
+                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                    CGPoint(x: width - spacing, y: height - spacing),
+                    CGPoint(x: spacing, y: height - spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight + spacing)
+                ])
             }
-            .fill(.red)
             
-            Path{ path in
-                path.move(to: CGPoint(x: width / 2, y: topHeight + (height - topHeight) / 2))
-                
-                let bottom = [
-                    CGPoint(
-                        x: 0.5 * width,
-                        y: 0.0 * (height - topHeight) + topHeight
-                    ),
-                    CGPoint(
-                        x: 0.75 * width,
-                        y: 0.25 * (height - topHeight) + topHeight
-                    ),
-                    CGPoint(
-                        x: 1.0 * width,
-                        y: 1.0 * (height - topHeight) + topHeight
-                    ),
-                    CGPoint(
-                        x: 0.0 * width,
-                        y: 1.0 * (height - topHeight) + topHeight
-                    ),
-                    CGPoint(
-                        x: 0.25 * width,
-                        y: 0.25 * (height - topHeight) + topHeight
-                    ),
-                ]
-                
-                bottom.forEach{ point in
-                    path.addLine(to: point)
-                }
-                
-                path.addLine(to: bottom[0])
-            }
-            .fill(.red)
+            .fill(Self.symbolColor)
         }
     }
+    
+    static let symbolColor = Color(
+        red: 79.0 / 255,
+        green: 79.0 / 255,
+        blue: 191.0 / 255
+    )
 }
 
 #Preview {
