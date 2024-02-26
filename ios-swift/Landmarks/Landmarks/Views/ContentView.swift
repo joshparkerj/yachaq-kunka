57474483437056cb48793532c1f5e9f614f7f677
Josh Parker <joshBBparker@gmail.com>
2024-02-26 10:43:47 -0700

    add the tab navigation

--- ios-swift/Landmarks/Landmarks/Views/ContentView.swift
+++ ios-swift/Landmarks/Landmarks/Views/ContentView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tab : Tab = .featured
+    enum Tab {
+        //        case hike
+        //        case category
+        //        case landmark
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
+//        switch tab {
+//        case Tab.list: LandmarkList()
+//        case Tab.featured: CategoryHome()
+//        }
+        TabView (selection: $tab) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
     }
 }
 
---
+++ ios-swift/Landmarks/Landmarks/Views/Tab.swift
@@ -0,0 +1,18 @@
+//
+//  Tab.swift
+//  Landmarks
+//
+//  Created by Joshua Parker on 2/26/24.
+//
+
+import SwiftUI
+
+struct Tab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tab()
+}
